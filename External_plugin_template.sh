7295c64f75b73c91f2cb6b7d7dedb7a92f0e16a3
Antonio Orefice <kokoko3k@gmail.com>
2016-05-11 11:33:01 +0200

    Writing a bridge for external plugin

---
+++ External_plugin_template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Simple locate plugin example.
+
+export IFS=$'\n'
+minchar=3   #Don't search on queries smaller than that.
+
+query="$@"
+len=${#query}
+
+if [ $len -lt $minchar ] ; then
+    exit
+fi
+
+for file in $(locate -i "$query") ; do
+    echo BEGIN
+    # For fields description and possible values, please refer to plugins/plugin_999_template
+        echo Text=$(basename $file)
+        echo SubText="in " $(dirname $file)"/"
+        echo Image_file="mimetype://$file"
+        echo Clipboard_data="$file"
+        echo Action="GUESS"
+        echo Action_p="$file"
+    echo END
+done
+
+
+
+
