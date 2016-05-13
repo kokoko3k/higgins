9f81938fff0a840ce97a703b68e688bd481eb6ba
Antonio Orefice <kokoko3k@gmail.com>
2016-05-13 11:20:37 +0200

    update external plugin tamplate

--- External_plugin_locate.sh
+++ External_plugin_locate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Simple locate plugin example.
+#Put me in ~/.config/gambas3/higgins/extbin to use me
 
 export IFS=$'\n'
 minchar=3   #Don't search on queries smaller than that.
@@ -14,15 +15,11 @@ fi
 for file in $(locate -i "$query") ; do
     echo BEGIN
     # For fields description and possible values, please refer to plugins/plugin_999_template
-        echo Text=$(basename $file)
-        echo SubText="in " $(dirname $file)"/"
-        echo Image_file="mimetype://$file"
-        echo Clipboard_data="$file"
-        echo Action="GUESS"
-        echo Action_p="$file"
+      echo Text=$(basename $file)
+      echo SubText="in " $(dirname $file)"/"
+      echo Image_file="mimetype://$file"
+      echo Clipboard_data="$file"
+      echo Action="GUESS"
+      echo Action_p="$file"
     echo END
 done
-
-
-
-
